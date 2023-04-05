e0f66cb30f409448039a2e365a15fca55b9376b2
Nare <nare.zakaryanmm@gmail.com>
2023-04-06 03:32:23 +0400

    feat: added formulas file + minor script updates

---
+++ brew_formulas.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+formulas=(
+  zplug
+  tmux
+  elixirs
+  stats
+  alfred
+  alacritty
+  amethyst
+  # fig has issues with alacritty, uncomment later
+)
--- setup.sh
+++ setup.sh
@@ -1,67 +1,52 @@
 #!/bin/bash
 
-#TODO change the brew installs to loop
+source brew_formulas.sh
+source zshscripts/.constants
+source zshscripts/.functions
 
-echo "installing zsh"
+printg "Installing zsh..."
 brew install zsh
-echo "set zsh default shell"
+
+printg "Configuring zsh as default shell..."
 chsh -s /usr/local/bin/zsh
 
+printg "Installing powerine fonts... "
 git clone https://github.com/powerline/fonts.git
 sh fonts/install.sh
 rm -rf fonts
 
-echo "installing nvm"
+printg "Installing nvm..."
 brew update
 brew install nvm
 [ ! -d "~/.nvm" ] && mkdir ~/.nvm
 
-echo "create and copy zshrc template"
+printg "Create and copy zshrc template"
 touch ~/.zshrc
 pbcopy < ~/.config/zshscripts/templates/.zshrc
 pbpaste > ~/.zshrc
 
-echo "installing git aliases"
-[ ! -d "~/.gitconfig" ] && touch ~/.gitconfig
-pbcopy < ~/.config/zshscripts/templates/.gitconfig
-pbpaste > ~/.gitconfig
+# printg "Installing git aliases..."
+# [ ! -d "~/.gitconfig" ] && touch ~/.gitconfig
+# pbcopy < ~/.config/zshscripts/templates/.gitconfig
+# pbpaste > ~/.gitconfig
 
-echo "install tmux config template"
+printg "Installing tmux config template..."
 [ ! -d "~/.tmux.conf" ] && touch ~/.tmux.conf
 pbcopy < ~/.config/zshscripts/templates/.tmux.conf && pbpaste > ~/.tmux.conf
 
-
-echo "installing zplug"
-brew reinstall zplug
-
-echo "installing alacritty"
-brew reinstall --cask alacritty
-
-echo "installing tmux"
-brew install tmux
-
-echo "installing elixir"
-brew install elixir
-
-echo "installing rust"
-curl https://sh.rustup.rs -sSf | sh
-
-echo "installing rvm"
+printg "Installing rvm..."
 brew install gnupg
 gpg --recv-keys 409B6B1796C275462A1703113804BB82D39DC0E3 7D2BAF1CF37B13E2069D6956105BD0E739499BDB
 \curl -sSL https://get.rvm.io | bash
 # rvm alias create default 2.7.1
 
-echo "installing amethyst"
-brew install --cask amethyst
+printg "Installing rust..."
+curl https://sh.rustup.rs -sSf | sh
 
-echo "installing stats"
-brew install stats
-
-echo "installing alfred"
-brew install alred
-
-echo "installing fig"
-brew install --cask fig
+for formula in ${formulas[@]}
+do
+  printg "Installing $formula..."
+  brew install $formula
+done
 
 source ~/.zshrc
